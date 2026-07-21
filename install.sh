c5599bbc87ba31d7d183964f8274bf2cc1c91fbd
b4zh <b4zh@tuta.io>
2026-07-21 20:34:47 +0200

    Updated install.sh. for

--- install.sh
+++ install.sh
@@ -100,7 +100,6 @@ sudo bash -c "echo 'es_ES.UTF-8 UTF-8' > /etc/locale.gen"
 sudo locale-gen
 
 ### Renombrando directorios
-cd ~
 dirs_en=$(grep "^XDG" ~/.config/user-dirs.dirs | cut -d"/" -f2 | sed 's/"//g' | xargs)
 num_dirs=$(echo $dirs_en | wc -w)
 LC_ALL=es_ES.UTF-8 xdg-user-dirs-update --force
@@ -111,5 +110,5 @@ dir2=
 for i in $(seq 1 $num_dirs); do
 	dir1=$(printf "%s\n%s" "$dirs_en" "$dirs_es" | cut -d" " -f$i | xargs | awk '{print $1}')
 	dir2=$(printf "%s\n%s" "$dirs_en" "$dirs_es" | cut -d" " -f$i | xargs | awk '{print $2}')
-	rmdir "$dir1" > /dev/null 2>&1 || mv -v $dir1/* "$dir2" && rmdir "$dir1"
+	rmdir ~/$dir1 > /dev/null 2>&1 || mv -v ~/$dir1/* "~/$dir2" && rmdir ~/$dir1
 done;
